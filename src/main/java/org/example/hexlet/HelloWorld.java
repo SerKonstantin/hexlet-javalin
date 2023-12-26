aa50ebd2f484f9dcfffee18d0a16057fbdba5e62
serebrianskii <serebrianskiik@gmail.com>
2023-12-26 17:21:54 +0100

    Try request parameters

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -3,12 +3,23 @@ package org.example.hexlet;
 import io.javalin.Javalin;
 
 public class HelloWorld {
-    public static void main(String[] args) {
+    public static Javalin getApp() {
         var app = Javalin.create(config -> {
             config.plugins.enableDevLogging();
         });
 
         app.get("/", ctx -> ctx.result("Hello World"));
+
+        app.get("/hello", ctx -> {
+            var name = ctx.queryParamAsClass("name", String.class).getOrDefault("World");
+            ctx.result("Hello, " + name + "!");
+        });
+
+        return app;
+    }
+
+    public static void main(String[] args) {
+        var app = getApp();
         app.start(7070);
     }
 }
