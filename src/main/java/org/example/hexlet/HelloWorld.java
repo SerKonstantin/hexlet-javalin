f7297be6f06ab3c9e777395d8dd8d4dbe8c45820
serebrianskii <serebrianskiik@gmail.com>
2024-01-04 14:25:42 +0100

    Add concrete course pagea

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -2,44 +2,61 @@ package org.example.hexlet;
 
 import io.javalin.Javalin;
 import io.javalin.http.NotFoundResponse;
+import org.example.hexlet.dto.CoursePage;
 import org.example.hexlet.dto.CoursesPage;
 import org.example.hexlet.dto.UserPage;
 import org.example.hexlet.dto.UsersPage;
+import org.example.hexlet.model.Course;
+import org.example.hexlet.model.User;
 import org.example.hexlet.pseudoDatabases.CoursesList;
 import org.example.hexlet.pseudoDatabases.UsersList;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class HelloWorld {
+    private static final List<Course> COURSES = new CoursesList().getCourses();
+    public static final List<User> USERS = new UsersList().getUsers();
+
     public static Javalin getApp() {
         var app = Javalin.create(config -> config.plugins.enableDevLogging());
 
         app.get("/courses", ctx -> {
-            var courses = new CoursesList().getCourses();
             var term = ctx.queryParam("term");
             if (term != null) {
-                var filteredCourses = courses.stream()
+                var filteredCourses = COURSES.stream()
                         .filter(c -> c.getName().toLowerCase().contains(term.toLowerCase()))
                         .collect(Collectors.toList());
                 var page = new CoursesPage(filteredCourses);
                 ctx.render("courses/index.jte", Collections.singletonMap("page", page));
             } else {
-                var page = new CoursesPage(courses);
+                var page = new CoursesPage(COURSES);
                 ctx.render("courses/index.jte", Collections.singletonMap("page", page));
             }
         });
 
+        app.get("/courses/{id}", ctx -> {
+            var id = ctx.pathParamAsClass("id", Long.class).get();
+            var course = COURSES.stream()
+                    .filter(c -> id.equals(c.getId()))
+                    .findFirst()
+                    .orElse(null);
+            if (course == null) {
+                throw new NotFoundResponse("User not found");
+            }
+            var page = new CoursePage(course);
+            ctx.render("courses/show.jte", Collections.singletonMap("page", page));
+        });
+
         app.get("/users", ctx -> {
-           var users = new UsersList().getUsers();
-           var page = new UsersPage(users);
+           var page = new UsersPage(USERS);
            ctx.render("users/index.jte", Collections.singletonMap("page", page));
         });
 
         app.get("/users/{id}", ctx -> {
             var id = ctx.pathParamAsClass("id", Long.class).get();
-            var users = new UsersList().getUsers();
-            var user = users.stream()
+            var user = USERS.stream()
                     .filter(u -> id.equals(u.getId()))
                     .findFirst()
                     .orElse(null);
